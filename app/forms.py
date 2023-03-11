a2e405c3711fb906eff43047e87a67c8ae44247c
Kali Soutar <kalisoutar@gmail.com>
2023-03-10 21:08:55 -0500

    Added css and new properties

--- app/forms.py
+++ app/forms.py
@@ -6,9 +6,9 @@ from flask_wtf.file import FileAllowed, FileRequired
 class PropertyForm(FlaskForm):
     title = StringField('Property Title', validators=[DataRequired()])
     description = TextAreaField('Description', validators=[DataRequired()])
-    rooms = TextAreaField('No. of Rooms', validators=[DataRequired()])
-    bathrooms = TextAreaField('No. of Bathrooms', validators=[DataRequired()])
-    price = TextAreaField('Price', validators=[DataRequired()])
+    rooms = StringField('No. of Rooms', validators=[DataRequired()])
+    bathrooms = StringField('No. of Bathrooms', validators=[DataRequired()])
+    price = StringField('Price', validators=[DataRequired()])
     ptype = SelectField('Property Type', choices=[('House', 'House'), ('Apartment','Apartment')], validators=[DataRequired()])
     location = StringField('Location', validators=[DataRequired()])
     photo = FileField('Photo',validators=[FileRequired(),FileAllowed(['png','jpg',],"Only upload images.")])
